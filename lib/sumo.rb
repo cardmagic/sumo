f59b86e84d976df21725143de30e98048b856ce5
Lucas Carlson <lucas@rufy.com>
2010-06-18 01:15:44 -0700

    typo

--- lib/sumo.rb
+++ lib/sumo.rb
@@ -78,7 +78,7 @@ class Sumo
 	
 	def format_volume(volume, instance, device, mountpoint)
 		commands = [
-			"if [ -f #{device}1 ]; then mount #{device}1 #{mountpoint}; else echo ',,L' | sfdisk #{device} && mkfs.ext3 /dev/#{device}1 && mount #{device}1 #{mountpoint}; fi"
+			"if [ -f /dev/#{device}1 ]; then mount /dev/#{device}1 #{mountpoint}; else echo ',,L' | sfdisk /dev/#{device} && mkfs.ext3 /dev/#{device}1 && mount /dev/#{device}1 #{mountpoint}; fi"
 		]
 		ssh(instance, commands)
   end
