978fe1c5cbb6a47a9f8d0a1129764f9357b20b9b
Lucas Carlson <lucas@rufy.com>
2010-06-20 01:56:00 -0700

    Fixing where to look for roles

--- lib/sumo.rb
+++ lib/sumo.rb
@@ -199,7 +199,7 @@ class Sumo
 	def setup_role(hostname, role)
 		commands = [
 			"cd chef-cookbooks",
-			"/var/lib/gems/1.8/bin/chef-solo -c config.json -j dna/#{role}.json"
+			"/var/lib/gems/1.8/bin/chef-solo -c solo.rb -j roles/#{role}.json"
 		]
 		ssh(hostname, commands)
 	end
